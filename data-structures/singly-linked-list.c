5345deba99aac1322a8db4b4b4aea13b4fe5b0ab
Qui pe heo <plhqui5526@gmail.com>
2026-06-07 14:21:02 +0700

    Added length function

--- data-structures/singly-linked-list.c
+++ data-structures/singly-linked-list.c
@@ -7,3 +7,17 @@ typedef struct ListNode {
     struct ListNode* next;
 } ListNode;
 
+int length(ListNode *head) {
+    ListNode *current;
+    current = head;
+
+    int count=0;
+
+    while(current!=NULL) {
+        count++;
+        
+        current = current->next;
+    }
+
+    return count;
+}
