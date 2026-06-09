af81d49b8ec7044e9fb143666e8c733551926f30
Qui pe heo <plhqui5526@gmail.com>
2026-06-09 19:39:31 +0700

    added insert function

--- data-structures/doubly-linked-list.c
+++ data-structures/doubly-linked-list.c
@@ -4,7 +4,44 @@
 
 typedef struct ListNode {
     dataType data;
-    struct ListNode* previous;
+    struct ListNode* prev;
     struct ListNode* next;
 } ListNode;
 
+void InsertDoublyLinkedList(ListNode **head, dataType data, int position) {
+    ListNode *p;
+    int k=1;
+
+    ListNode* newNode = (ListNode*)malloc(sizeof(ListNode));
+
+    if(!newNode) {
+        printf("Memory error");
+        return;
+    }
+
+    if(position==1) {
+        newNode->prev = NULL;
+        newNode->next = p;
+        if(*head) (*head)->prev = newNode;
+        *head = newNode;
+        return;
+    }
+    p = *head;
+    while((k < position-1) && (p->next!=NULL)) {
+        p=p->next;
+        k++;
+    }
+
+    if(k!=position) {
+        printf("Position does not exist");
+        return;
+    }
+
+    newNode->prev = p;
+    newNode->next = p->next;
+    
+    if(p->next) p->next->prev = newNode;
+
+    p->next = newNode;
+    return;
+}
