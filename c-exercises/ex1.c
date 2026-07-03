ffde0098ff2a2524200316c2a509f4f5497e5714
Qui pe heo <plhqui5526@gmail.com>
2026-07-03 18:24:15 +0700

    added one array program

---
+++ c-exercises/ex1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void printArray(const int array[], int length) {
+    printf("[ ")
+    for(int i = 0; i < length; i++) {
+        printf("%d", );
+        if(i != length - 1) printf(", ");
+    }
+    printf(" ]\n");
+}
+
+int main() {
+    int array[] = {7, 2, 3, 5, 8};
+    int length = sizeof(array)/sizeof(array[0]);
+
+    printArray(array, length);
+
+    return 0;
+}
