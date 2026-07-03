4916e73d4c2843f05bdc81b9c3e613c19cdbb61c
Qui pe heo <plhqui5526@gmail.com>
2026-07-03 18:30:25 +0700

    readjusted the ex1 code

--- c-exercises/ex1.c
+++ c-exercises/ex1.c
@@ -9,11 +9,26 @@ void printArray(const int array[], int length) {
     printf("]\n");
 }
 
+void findPair(const int array[], int length, int sum) {
+    for(int i = 0; i < length; i++) {
+        for(int j = 0; j < length; j++) {
+            if((i != j) && (array[i] + array[j] == sum)) {
+                printf("Pair found (%d, %d)\n", array[i], array[j]);
+            }
+        }
+    }
+}
+
 int main() {
     int array[] = {7, 2, 3, 5, 8};
     int length = sizeof(array)/sizeof(array[0]);
 
     printArray(array, length);
 
+    int sum;
+    printf("What is the sum you want to input?   ");
+    scanf("%d", &sum);
+
+    findPair(array, length, sum);
     return 0;
 }
