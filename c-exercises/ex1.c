a7de698f5aaed001da38576cc9cd499f80a8ed1a
Qui pe heo <plhqui5526@gmail.com>
2026-07-03 18:45:11 +0700

    made adjustments

--- c-exercises/ex1.c
+++ c-exercises/ex1.c
@@ -14,9 +14,43 @@ void findPair(const int array[], int length, int sum) {
         for(int j = 0; j < length; j++) {
             if((i != j) && (array[i] + array[j] == sum)) {
                 printf("Pair found (%d, %d)\n", array[i], array[j]);
+                return;
             }
         }
     }
+    printf("No pair found.\n");
+}
+
+void insertionSort(int array[], int length) {
+    for(int i = 0; i < length; i++) {
+        int key = array[i];
+        int j = i - 1;
+
+        while(j >= 0 && array[j] > key) {
+            array[j+1] = array[j];
+
+            j--;
+        }
+
+        array[j+1] = key; 
+    }
+}
+
+void findPairSort(int array[], int length, int sum) {
+    insertionSort(array, length);
+
+    int lowindex = 0;
+    int highindex = length - 1;
+
+    while(lowindex < highindex) {
+        if(array[lowindex] + array[highindex] == sum) {
+            printf("Found pair (%d, %d)\n", array[lowindex], array[highindex]);
+            return;
+        }
+        if(array[lowindex] + array[highindex] > sum) highindex--;
+        if(array[lowindex] + array[highindex] < sum) lowindex++;
+    }
+    printf("No pair found.\n");
 }
 
 int main() {
@@ -29,6 +63,6 @@ int main() {
     printf("What is the sum you want to input?   ");
     scanf("%d", &sum);
 
-    findPair(array, length, sum);
+    findPairSort(array, length, sum);
     return 0;
 }
