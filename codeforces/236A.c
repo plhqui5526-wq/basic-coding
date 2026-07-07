83cbeaa2c5744c9d07e537d0b5c27fc6302025ba
Qui pe heo <plhqui5526@gmail.com>
2026-07-07 21:04:24 +0700

    fixed 236A but to no avail

--- codeforces/236A.c
+++ codeforces/236A.c
@@ -5,25 +5,26 @@ int main() {
 
     scanf(" %s", &string);
 
-    int duplicate = 0;
+    int seen[100] = {0};
+    int distinctCount = 0;
     int counter = 0;
 
     for(int i = 0; string[i] != '\0'; i++) {
-        for(int j = i; string[j] != '\0'; j++) {
-            if(string[i] == string[j] && j != i) {
-                duplicate++;
-            }
+        unsigned char ch = (unsigned char)string[i];
 
+        if(seen[ch] == 0) {
+            seen[ch] = 1;
+            distinctCount++;
+            printf("Incremented ascii %d\n", ch);
         }
 
         counter++;
     }
 
-    int test = counter - duplicate;
-
-    if(test % 2 == 0) printf("CHAT WITH HER!");
+    if(distinctCount % 2 == 0) printf("CHAT WITH HER!");
     else printf("IGNORE HIM!");
 
-    printf(" %d %d", test, counter);
+    printf(" %d %d", counter, distinctCount);
+
     return 0;
 }
