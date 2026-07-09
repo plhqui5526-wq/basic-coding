ee9a728ddaadfba89f0a0b591c3f4024ccb96b24
Qui pe heo <plhqui5526@gmail.com>
2026-07-09 18:12:26 +0700

    did 791A

--- codeforces/791A.c
+++ codeforces/791A.c
@@ -2,5 +2,15 @@
 
 int main() {
     int a, b;
-    scanf("")
+    int counter=0;
+    scanf("%d%d", &a, &b);
+
+    while(a <= b) {
+        a *= 3;
+        b *=2;
+        counter++;
+    }
+
+    printf("%d", counter);
+    return 0;
 }
