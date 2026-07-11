af42741a961d398c18af28b0fe3aa59732749a41
Qui pe heo <plhqui5526@gmail.com>
2026-07-11 07:52:54 +0700

    did 59A

---
+++ codeforces/59A.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<ctype.h>
+
+void lowerr(char string[]) {
+    for(int i = 0; i != '\0'; i++) {
+        string[i] = (char)tolower((unsigned char)string[i]);
+    }
+}
+
+void upperr(char string[]) {
+    for(int i = 0; i != '\0'; i++) {
+        string[i] = (char)toupper((unsigned char)string[i]);
+    }
+}
+
+int main() {
+    char string[101];
+    scanf("%s", string);
+
+    int up = 0; int low = 0;
+
+    for(int i = 0; i != '\0'; i++) {
+        if(string[i] >= 'A' && string[i] <= 'Z') up++;
+        else if(string[i] >= 'a' && string[i] <= 'z') low++;
+    }
+
+    if(low >= up) lowerr(string);
+    else upperr(string);
+
+    return 0;
+}
