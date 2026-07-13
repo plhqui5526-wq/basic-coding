32a0e07fdf3d0f8c5a40b59a54a3e38c549894f6
Qui pe heo <plhqui5526@gmail.com>
2026-07-13 20:18:45 +0700

    did 110A but wrong

--- codeforces/800/110A.c
+++ codeforces/800/110A.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
 
 int main() {
-    
+    long long int n;
+    scanf("%lld", &n);
+    int digits = 0;
+
+    if(n % 10 == 7 || n % 10 == 4) printf("YES");
+    else {
+        while(n >= 0) {
+            n = n / 10;
+            digits++;
+        }
+
+        if(digits % 10 == 4 || digits % 10 == 7) printf("YES");
+        else printf("NO");
+    }
+
+    return 0;
 }
