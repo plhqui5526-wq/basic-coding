c6f23ca37eb1e3df5cb9f0c5ba6e0231783b922a
Qui pe heo <plhqui5526@gmail.com>
2026-07-14 08:23:18 +0700

    solved 110A

--- codeforces/800/110A.c
+++ codeforces/800/110A.c
@@ -4,12 +4,13 @@ int main() {
     long long int n;
     scanf("%lld", &n);
     int digits = 0;
+    int digit;
     
     while(n > 0) {
+        digit = n % 10;
+        if(digit == 4 || digit == 7) digits++;
         n = n / 10;
-        digits++;
     }
-
     if(digits % 10 == 4 || digits % 10 == 7) printf("YES");
     else printf("NO");
 
