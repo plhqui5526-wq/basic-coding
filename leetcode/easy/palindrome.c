bb81a10b776fdaef7eebf59a7706e6b165928d38
Qui pe heo <plhqui5526@gmail.com>
2026-07-17 19:03:30 +0700

    solved palindrome

--- leetcode/easy/palindrome.c
+++ leetcode/easy/palindrome.c
@@ -1,8 +1,24 @@
 #include<stdio.h>
 #include<stdbool.h>
+#include<string.h>
 
 bool isPalindrome(int x) {
+    char array[12];
+
+    snprintf(array, sizeof(array), "%d", x);
+
+    int left = 0;
+    int right = strlen(array) - 1;
     
+    bool flag = true;
+    while(left < right) {
+        if(array[left] != array[right]) flag = false;
+
+        left++;
+        right--;
+    }
+
+    return flag;
 }
 
 int main() {
