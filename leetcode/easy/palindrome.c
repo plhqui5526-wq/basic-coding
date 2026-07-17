421051742e03f15648dcb33fe1747ee25ada40c3
Qui pe heo <plhqui5526@gmail.com>
2026-07-17 18:47:56 +0700

    begin redoing palindrome

--- leetcode/easy/palindrome.c
+++ leetcode/easy/palindrome.c
@@ -2,31 +2,7 @@
 #include<stdbool.h>
 
 bool isPalindrome(int x) {
-    int t = x;
-    int n = 0;
-    while(t > 0) {
-        t = t / 10;
-        n++;
-    }
     
-    int array[n];
-    int j = 0;
-
-    t = x;
-    while(t > 0) {
-        array[j] = t % 10;
-        t = t / 10;
-        printf("%d ", array[j]);
-        j++;
-    }
-    bool flag = true;
-    for(int i = n - 1; i >= 0; i--) {
-        if(array[i] < 0) continue;
-        if(array[i] != x % 10) flag = false; 
-        x = x / 10;
-    }
-
-    return flag;
 }
 
 int main() {
