c7decc8881c12d6404a46bda91712e13986136a7
Qui pe heo <plhqui5526@gmail.com>
2026-07-19 19:40:31 +0700

    did 271 but didn't figure

---
+++ codeforces/800/271A.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+int check(int y) {
+    int array[4];
+
+    snprintf(array, sizeof(array), "%d", y);
+
+
+}
+
+int main() {
+    int y;
+    scanf("%d", &y);
+    int flag = 1;
+    int y1 = y + 1;
+    while(flag) {
+        
+    }
+    return 0;
+}
