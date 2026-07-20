2c835503ba1c84234ce78e59ec2eb719de8e4bb7
Qui pe heo <plhqui5526@gmail.com>
2026-07-20 08:39:43 +0700

    solved 271A

--- codeforces/800/271A.c
+++ codeforces/800/271A.c
@@ -1,21 +1,34 @@
 #include<stdio.h>
-#include<string.h>
+#include<stdbool.h>
 
-int check(int y) {
-    int array[4];
+bool check(int i) {
+    int seen[10] = {0};
 
-    snprintf(array, sizeof(array), "%d", y);
+    while(i > 0) {
+        int num = i % 10;
+        seen[num]++;
+        i /= 10;
+    }
 
+    for(int j = 0; j < 10; j++) {
+        if(seen[j] > 1) {
+            return false;
+        }
+    }
 
+    return true;
 }
 
 int main() {
     int y;
     scanf("%d", &y);
     int flag = 1;
-    int y1 = y + 1;
-    while(flag) {
-        
+    for(int i = y + 1; i <= 9012; i++) {
+        if(check(i)) {
+            printf("%d", i);
+            break;
+        }
     }
+    
     return 0;
 }
